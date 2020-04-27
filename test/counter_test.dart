b87224bb28eec61581a35fd72f76fd4394276f78
Yvon <ym2637@columbia.edu>
2020-04-27 10:59:33 +0300

    unit tests using test library and some higher level functions. am considering goin into test driven dev. will see

---
+++ test/counter_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_testing/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("Counter", () {
+    test("value should start at 0", () {
+      expect(Counter().value, 0);
+    });
+
+    test("value should be decremented", () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
+  });
+
+  test("Counter value should be incremented", () {
+    final counter = Counter();
+
+    counter.increment();
+    expect(counter.value, 1);
+  });
+}
